98f3156e2bfddb289411d698e64bba074ba69156
Morgan Williams <morgan.williams.ext@allianz-trade.com>
2025-05-07 00:21:10 +0200

    WIP multi translator language assignment

--- src/lang_utils.js
+++ src/lang_utils.js
@@ -1,4 +1,6 @@
 import wikidataLanguages from './wikidata-languages.json';
+import deeplTargets from './deepl-targets.json';
+import m2mSupport from './m2m-support.json';
 
 export const ISO3_TO_ISO2_MAP = wikidataLanguages.reduce((acc, lang) => {
     if (lang.iso && lang.iso1) {
@@ -20,4 +22,32 @@ export function getISO2ForModel(iso3) {
 
 export function getISO3FromISO2(iso2) {
     return ISO2_TO_ISO3_MAP[iso2.toUpperCase()] || null;
+}
+
+// Build sets of supported 3-letter codes for each translator
+const deeplSupported = new Set(
+  deeplTargets.map(l => l.iso3 || l.language.toLowerCase())
+);
+const m2mSupported = new Set(Object.keys(m2mSupport));
+const openaiSupported = new Set(
+  wikidataLanguages.map(l => l.iso).filter(Boolean)
+);
+
+export function assignTranslators(tgt_langs, translator_order = ['deepl', 'm2m', 'openai']) {
+  const result = { deepl: [], m2m: [], openai: [], unsupported: [] };
+  const supportMap = {
+    deepl: deeplSupported,
+    m2m: m2mSupported,
+    openai: openaiSupported
+  };
+
+  for (const code of tgt_langs) {
+    const assigned = translator_order.find(tr => supportMap[tr] && supportMap[tr].has(code));
+    if (assigned) {
+      result[assigned].push(code);
+    } else {
+      result.unsupported.push(code);
+    }
+  }
+  return result;
 }
